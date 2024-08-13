4b349099351bc5b6d0618e13afdeedfe53602513
Jorge Matos <jisapjisap@gmail.com>
2024-08-13 15:47:06 +0200

    setup & navbar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import HeroSection from './components/HeroSection'
+import Navbar from './components/Navbar'
+
+
+function App() {
+
+
+  return (
+    <main className="overflow-y-hidden text-neutral-200 antialiased">
+      <HeroSection />
+      <Navbar />
+    </main>
+  )
+}
+
+export default App
